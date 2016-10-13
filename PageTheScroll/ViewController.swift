5104fc2955a41c63aca5d34f927c58ddb1116479
Jawad Shuaib <jawadshuaib@Jawads-MacBook-Pro.local>
2016-10-13 12:35:16 -0400

    Added bleeding images to the view port

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -18,6 +18,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // scrollView frame data (such as the Scroll View width) is not available
+        // here so we move it to viewDidAppear. Some info on UI objects does not
+        // get loaded when the viewDidLoad function runs
+        
+    }
+
+    // Move code in this block because the UIScrollView is guaranteed to have
+    // loaded in the following block
+    override func viewDidAppear(_ animated: Bool) {
         // We will need to set the content size window inside the scroll view
         var contentWidth: CGFloat = 0.0
         
@@ -36,7 +45,11 @@ class ViewController: UIViewController {
             // midX is the value of x in the middle of the screen
             // frame.size.width is the width of the entire screen
             
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+//            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            
+            let scrollWidth = scrollView.frame.size.width
+            
+            newX = scrollWidth/2 + scrollWidth * CGFloat(x)
             
             contentWidth += newX
             
@@ -45,17 +58,17 @@ class ViewController: UIViewController {
             
             // Now we can set the position of the image inside the
             //Scroll view
-            imageView.frame = CGRect (x: newX - 75, y: (view.frame.size.height/2) - 75, width: 150, height: 150)
-        
+//            imageView.frame = CGRect (x: newX - 75, y: (view.frame.size.height/2) - 75, width: 150, height: 150)
+            imageView.frame = CGRect (x: newX - 75, y: (scrollView.frame.size.height/2) - 75, width: 150, height: 150)
+            
+//            scrollView.backgroundColor = UIColor.purple
+            // Don't hide the images behind the scroll view:
+            scrollView.clipsToBounds = false
+            
             // We need to set the content size within the scroll view
             // so that the scroll view knows how far to scroll
             scrollView.contentSize = CGSize (width: contentWidth, height: view.frame.size.height)
         }
-        
-        
-        print ("Count: \(images.count)")
-        
     }
-
 }
 
